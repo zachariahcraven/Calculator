d098db11a01dee470f2efd55d4cbc4bc88116027
zachariahcraven <118693079+zachariahcraven@users.noreply.github.com>
2024-07-16 17:45:04 -0600

    update to ui javascript instead of html

--- javascript.js
+++ javascript.js
@@ -36,15 +36,50 @@ function operate(numberOne, operater, numberTwo) {
 var operater = "";
 var numberOne = "";
 var numberTwo = "";
-var displayContent = "";
+var displayContent = "empty";
 
 //main
-const calculatorButtons = document.querySelectorAll("button");
-const display = document.querySelector("#display");
-const caculator = document.querySelector("#calculator");
+//Creating UI
+const body = document.querySelector("body");
+const containor = document.createElement("calculator");
+const display = document.createElement("display");
+const keypad = document.createElement("keypad");
+const leftDiv = document.createElement("leftPad");
 
-display.textContent = displayContent;
+const rightDiv = document.createElement("rightPad");
+const calculatorButtons = [];
+
+for (let i = 0; i < 10; i++) {
+    let button = document.createElement("button");
+    button.textContent= i;
+    calculatorButtons.push(button);
+}
+
+let clear = document.createElement("button");
+clear.textContent = "AC";
+
+let equal = document.createElement("button");
+equal.textContent = "=";
+
+let division = document.createElement("button");
+division.textContent = "/";
+division.style.backgroundColor = "coral";
+
+let multipication = document.createElement("button");
+multipication.textContent = "*";
+multipication.style.backgroundColor = "coral";
+
+let subtraction = document.createElement("button");
+subtraction.textContent = "-";
+subtraction.style.backgroundColor = "coral";
+
+let addition = document.createElement("button");
+addition.textContent = "+";
+addition.style.backgroundColor = "coral";
+
+//add event code to each button
 calculatorButtons.forEach(button => {
+    //event code
     button.addEventListener("click", () => {
         if (parseInt(button.textContent) !== NaN) {
             if (numberOne !== null) {
@@ -52,6 +87,25 @@ calculatorButtons.forEach(button => {
                 displayContent += parseInt(button.textContent);
             }
         }
-        //save operator
     })
 })
+//build ui (append divs)
+containor.appendChild(display);
+for (let i = 1; i < calculatorButtons.length; i++) {
+    leftDiv.appendChild(calculatorButtons[i]);
+}
+leftDiv.appendChild(calculatorButtons[0]);
+leftDiv.appendChild(clear);
+leftDiv.appendChild(equal);
+
+rightDiv.appendChild(division);
+rightDiv.appendChild(multipication);
+rightDiv.appendChild(subtraction);
+rightDiv.appendChild(addition);
+
+keypad.appendChild(leftDiv);
+keypad.appendChild(rightDiv);
+
+containor.appendChild(keypad);
+
+body.appendChild(containor);
