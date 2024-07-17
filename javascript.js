ea72574ffce691b8b4ee40e039c0c7b442bb0b2b
zachariahcraven <118693079+zachariahcraven@users.noreply.github.com>
2024-07-16 19:24:24 -0600

    funtionality for calculator

--- javascript.js
+++ javascript.js
@@ -18,25 +18,46 @@ function divide(numberOne, numberTwo) {
 function operate(numberOne, operater, numberTwo) {
     switch (operater) {
         case "+":
-            add(numberOne, numberTwo);
-            break;
+            return add(numberOne, numberTwo);
         case "-":
-            subtract(numberOne, numberTwo);
-            break;
+            return subtract(numberOne, numberTwo);
         case "*":
-            multiply(numberOne, numberTwo);
-            break;
+            return multiply(numberOne, numberTwo);
         case "/":
-            divide(numberOne, numberTwo);
-            break;
+            return divide(numberOne, numberTwo);
     }
+}
+
+function reBuildUI() {
+    containor.remove(display);
+    display.textContent = displayContent;
+    containor.appendChild(display);
+    for (let i = 1; i < calculatorButtons.length; i++) {
+        leftDiv.appendChild(calculatorButtons[i]);
+    }
+    leftDiv.appendChild(calculatorButtons[0]);
+    leftDiv.appendChild(clear);
+    leftDiv.appendChild(equal);
+
+    rightDiv.appendChild(division);
+    rightDiv.appendChild(multipication);
+    rightDiv.appendChild(subtraction);
+    rightDiv.appendChild(addition);
+
+    keypad.appendChild(leftDiv);
+    keypad.appendChild(rightDiv);
+
+    containor.appendChild(keypad);
+
+    body.appendChild(containor);
 }
 
 //globalVarables--------------
 var operater = "";
 var numberOne = "";
 var numberTwo = "";
-var displayContent = "empty";
+var displayContent = "";
+var operaterActive = false;
 
 //main
 //Creating UI
@@ -45,9 +66,9 @@ const containor = document.createElement("calculator");
 const display = document.createElement("display");
 const keypad = document.createElement("keypad");
 const leftDiv = document.createElement("leftPad");
-
 const rightDiv = document.createElement("rightPad");
 const calculatorButtons = [];
+const operaterButtons = [];
 
 for (let i = 0; i < 10; i++) {
     let button = document.createElement("button");
@@ -64,31 +85,64 @@ equal.textContent = "=";
 let division = document.createElement("button");
 division.textContent = "/";
 division.style.backgroundColor = "coral";
+operaterButtons.push(division);
 
 let multipication = document.createElement("button");
 multipication.textContent = "*";
 multipication.style.backgroundColor = "coral";
+operaterButtons.push(multipication);
 
 let subtraction = document.createElement("button");
 subtraction.textContent = "-";
 subtraction.style.backgroundColor = "coral";
+operaterButtons.push(subtraction);
 
 let addition = document.createElement("button");
 addition.textContent = "+";
 addition.style.backgroundColor = "coral";
+operaterButtons.push(addition);
+//--------------------------------------------------
 
 //add event code to each button
 calculatorButtons.forEach(button => {
-    //event code
     button.addEventListener("click", () => {
-        if (parseInt(button.textContent) !== NaN) {
-            if (numberOne !== null) {
-                numberOne += parseInt(button.textContent);
-                displayContent += parseInt(button.textContent);
-            }
+        if (!operaterActive) {
+            numberOne += parseInt(button.textContent);
+        } else {
+            numberTwo += parseInt(button.textContent);
         }
+        displayContent = numberOne + operater + numberTwo;
+        reBuildUI();
     })
 })
+operaterButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        operaterActive = true;
+        operater = button.textContent;
+        displayContent = numberOne + operater + numberTwo;
+        reBuildUI();
+    })
+})
+equal.addEventListener("click", () => {
+    if (numberOne !== "" && numberTwo !== "") {
+        numberOne = operate(parseInt(numberOne), operater, parseInt(numberTwo));
+        numberTwo = "";
+        operater = "";
+        displayContent = numberOne + operater + numberTwo;
+        numberOne = ""
+        operaterActive = false;
+        reBuildUI();
+    }
+})
+clear.addEventListener("click", () => {
+    operater = "";
+    numberOne = "";
+    numberTwo = "";
+    displayContent = "";
+    operaterActive = false;
+    reBuildUI();
+})
+
 //build ui (append divs)
 containor.appendChild(display);
 for (let i = 1; i < calculatorButtons.length; i++) {
